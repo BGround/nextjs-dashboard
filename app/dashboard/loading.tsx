892091d3780694ea184b1474a2435f9d9d47ee4b
zhangya <874541450@qq.com>
2025-06-26 13:55:00 +0800

    chap8-skeletons

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+
+// step 1 add loging text in page
+// export default function Loading() {
+//   return <div>Loading...</div>;
+// }
+
+// step 2 add skeleton loading
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />
+}
